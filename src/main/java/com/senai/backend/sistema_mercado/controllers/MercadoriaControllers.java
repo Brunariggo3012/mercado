7d8cd0156272e367a7bf9190c5ab8ae48ea014f7
brunariggo3012 <brunapequena0909@gmail.com>
2025-11-14 14:13:16 -0300

    inserção dos controladores

--- src/main/java/com/senai/backend/sistema_mercado/controllers/MercadoriaControllers.java
+++ src/main/java/com/senai/backend/sistema_mercado/controllers/MercadoriaControllers.java
@@ -1,5 +1,31 @@
 package com.senai.backend.sistema_mercado.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.senai.backend.sistema_mercado.models.Mercadoria;
+import com.senai.backend.sistema_mercado.services.MercadoriaServices;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/mercadoria")
 public class MercadoriaControllers {
-    
+
+    @Autowired
+    private MercadoriaServices mercadoriaServices;
+
+    @PostMapping("/cadastrar")
+     public Mercadoria cadastrar(@RequestBody Mercadoria mercadoria){
+     return mercadoriaServices.cadastrar(mercadoria);
+    }
+
+    @GetMapping("/recuperar-por-id/{id}")
+     public Mercadoria recuperarPorId(@PathVariable Integer id){
+        return mercadoriaServices.recuperarPorId(id);
+     }
 }
