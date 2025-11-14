cf4c6eb1cb04d527ee83a09d2a1b7aad19fd260e
brunariggo3012 <brunapequena0909@gmail.com>
2025-11-14 16:37:21 -0300

    inserção no readme

--- src/main/java/com/senai/backend/sistema_mercado/models/Funcionario.java
+++ src/main/java/com/senai/backend/sistema_mercado/models/Funcionario.java
@@ -3,12 +3,11 @@ package com.senai.backend.sistema_mercado.models;
 import java.time.LocalDate;
 import java.util.List;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
